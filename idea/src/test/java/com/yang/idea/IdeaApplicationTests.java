f0dc00122f424da20624536c3416c14502a1f016
yang <yangchengjuily@163.com>
2019-02-28 20:33:38 +0800

    提交1

---
+++ idea/src/test/java/com/yang/idea/IdeaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yang.idea;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IdeaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
