7965784e232157da0bed40d7217f7492be770a5a
yang <yangchengjuily@163.com>
2019-02-28 20:57:29 +0800

    提交4

--- idea/src/main/java/com/yang/idea/ideaController.java
+++ idea/src/main/java/com/yang/idea/ideaController.java
@@ -1,4 +1,13 @@
 package com.yang.idea;
 
 public class ideaController {
+
+    public static void main(String[] args) {
+        System.out.println("zhngsdasagsg");
+        System.out.println("zhngsdasagsg");
+        System.out.println("zhngsdasagsg");
+        System.out.println("zhngsdasagsg");
+        System.out.println("zhngsdasagsg");
+    }
 }
+
